7d9ef554b7eeb22ba67a72b31c9c2e9d521fc955
Janatrix <202718648+0xJana@users.noreply.github.com>
2025-04-30 19:02:10 +0300

    Q9.c

---
+++ pl_sheet6/Q9.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int digits(char *str)
+{
+    if(*str == '\0'){
+        return 0;
+    }
+    else{
+         if(*str>=0){
+             return 1 + digits(str+1);
+        }
+        else{
+            return digits(str+1);
+        }
+}}
+int main(){
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    char *ptr = str;
+    if(digits(ptr)){
+        printf("The string contains digits\n");
+    }
+    else{
+        printf("The string does not contain digits\n");
+    }
+}
