0e4b9f8bd758559c78296b73234844e253274492
Janatrix <202718648+0xJana@users.noreply.github.com>
2025-04-30 15:40:23 +0300

    Create Q3.c

---
+++ pl_sheet6/Q3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+void calc(float numberOfMiles,float fuelConsumption,float *fuelConsumptionPerMile){
+    if(numberOfMiles == 0){
+        printf("Error: Division by zero\n");
+        return;
+    }
+    if(fuelConsumption < 0 || numberOfMiles < 0){
+        printf("Error: Negative values are not allowed\n");
+        return;
+    }
+    *fuelConsumptionPerMile = fuelConsumption / numberOfMiles;
+}
+int main(){
+    float numberOfMiles=0, fuelConsumption=0, fuelConsumptionPerMile=0;
+    printf("Enter the number of miles: ");
+    scanf("%f", &numberOfMiles);
+    printf("Enter the fuel consumption: ");
+    scanf("%f", &fuelConsumption);
+    calc(numberOfMiles, fuelConsumption, &fuelConsumptionPerMile);
+    printf("Fuel consumption per mile: %.2f\n", fuelConsumptionPerMile);
+}
